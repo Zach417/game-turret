26fa3a431be59ee8a690ffc091c12d04b08705d1
Zach Allen <zallen@pension-consultants.com>
2017-01-02 12:06:06 -0600

    moving target along y axis

--- target.js
+++ target.js
@@ -1,19 +1,37 @@
 function Target(x,y) {
-  this.x = x;
-  this.y = y;
-  this.marginOfError = 15;
+  this.pos = createVector(x,y);
+  this.vel = createVector(0,2);
+  this.directionChange = new Date();
+  this.marginOfError = 25;
 
   this.hit = function (x,y) {
-    var hitX = this.x < x + this.marginOfError && this.x > x - this.marginOfError;
-    var hitY = this.y < y + this.marginOfError && this.y > y - this.marginOfError;
+    var hitX = this.pos.x < x + this.marginOfError && this.pos.x > x - this.marginOfError;
+    var hitY = this.pos.y < y + this.marginOfError && this.pos.y > y - this.marginOfError;
     return hitX && hitY;
+  }
+
+  this.update = function () {
+    var len = (new Date() - this.directionChange);
+    if (len > 200) {
+      if (this.pos.y > height) {
+        this.vel = createVector(this.vel.x, -this.vel.y);
+        this.directionChange = new Date();
+      } else if (this.pos.y < 0) {
+        this.vel = createVector(this.vel.x, -this.vel.y);
+        this.directionChange = new Date();
+      } else if (random(0,1) > 0.995) {
+        this.vel = createVector(this.vel.x, -this.vel.y);
+      }
+    }
+
+    this.pos.add(this.vel);
   }
 
   this.show = function () {
     push();
     stroke('yellow');
     strokeWeight(25);
-    point(x,y);
+    point(this.pos.x,this.pos.y);
     pop();
   }
 }
